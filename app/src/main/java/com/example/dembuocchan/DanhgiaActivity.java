263b1a581d8a5d6c9ca8dd6d91cf85c7a27d0009
TuanAnh2502 <chuthienanh2502@gmail.com>
2023-11-08 19:28:00 +0700

    add len main

--- app/src/main/java/com/example/dembuocchan/DanhgiaActivity.java
+++ app/src/main/java/com/example/dembuocchan/DanhgiaActivity.java
@@ -6,7 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,19 +19,34 @@ import java.text.SimpleDateFormat;
 public class DanhgiaActivity extends AppCompatActivity {
     Button send;
     EditText edtDanhgia;
+    RatingBar rtbStar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danhgia);
         send=findViewById(R.id.buttonSend);
         edtDanhgia=findViewById(R.id.editTextDanhgia);
+        rtbStar=findViewById(R.id.ratingBar);
+        rtbStar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(DanhgiaActivity.this, "so sao"+(int)rtbStar.getRating(),Toast.LENGTH_SHORT).show();
+            }
+        });
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int sosao=(int)rtbStar.getRating();
+                if(sosao<1){
+                    Toast.makeText(DanhgiaActivity.this, "Vui lòng nhập số sao"+(int)rtbStar.getRating(),Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 LinearLayout danhgia=findViewById(R.id.xemdanhgia);
                 View userReviewLayout = LayoutInflater.from(DanhgiaActivity.this).inflate(R.layout.danhgiaform, null);
                 TextView userNameTextView = userReviewLayout.findViewById(R.id.textView19);
                 userNameTextView.setText("Tuấn Anh"); // Thay tên người dùng tương ứng
+                RatingBar userRating=userReviewLayout.findViewById(R.id.ratingBar3);
+                userRating.setRating(sosao);
                 EditText userReviewEditText = userReviewLayout.findViewById(R.id.editTextText2);
                 String userReviewText = edtDanhgia.getText().toString(); // Thay nội dung đánh giá tương ứng
                 userReviewEditText.setText(userReviewText);
@@ -37,6 +54,7 @@ public class DanhgiaActivity extends AppCompatActivity {
                 dateTextView.setText("Ngày: " + getCurrentDate()); // Sử dụng phương thức để lấy ngày tháng hiện tại
                 danhgia.addView(userReviewLayout, 0);
                 edtDanhgia.setText("");
+                rtbStar.setRating(0);
             }
         });
     }
